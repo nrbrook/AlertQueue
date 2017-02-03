e57a4f1d0eedd648b96778bdb8687c0f77108de6
Nick Brook <nrbrook@gmail.com>
2017-02-03 23:03:27 +0000

    Initial Commit

---
+++ AlertQueue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertQueue
+//
+//  Created by Nick Brook on 03/02/2017.
+//  Copyright © 2017 Nick Brook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlertQueue/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AlertQueue
+//
+//  Created by Nick Brook on 03/02/2017.
+//  Copyright © 2017 Nick Brook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
